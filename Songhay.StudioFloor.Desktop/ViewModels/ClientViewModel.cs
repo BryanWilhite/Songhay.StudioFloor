45ad9552919cd0b887da35a88851530328131abd
Bryan Wilhite <bryan.wilhite@gmail.com>
2017-04-18 13:50:15 -0700

    added sort to IndexView

--- Songhay.StudioFloor.Desktop/ViewModels/ClientViewModel.cs
+++ Songhay.StudioFloor.Desktop/ViewModels/ClientViewModel.cs
@@ -3,10 +3,13 @@ using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
 using Songhay.Models;
+using Songhay.Mvvm.Extensions;
 using Songhay.Mvvm.ViewModels;
 using Songhay.StudioFloor.Desktop.Common.Events;
 using Songhay.StudioFloor.Desktop.Common.Extensions;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace Songhay.StudioFloor.Desktop.ViewModels
 {
@@ -14,13 +17,9 @@ namespace Songhay.StudioFloor.Desktop.ViewModels
     {
         public ClientViewModel(IEventAggregator eventAggregator)
         {
-            this._regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
-            this._clientContentRegionNavigation = this.GetClientContentRegionNavigation(this._regionManager);
-
-            this.Index = new ObservableCollection<DisplayItemModel>();
-            eventAggregator
-                .GetEvent<ModuleInitializedEvent>()
-                .Subscribe(displayItem => this.Index.Add(displayItem));
+            this.SetupRegionNavigation();
+            this.SetupIndexViewSource();
+            this.SetupIndexWithEventing(eventAggregator);
         }
 
         public RegionNavigationViewModel ClientContentRegionNavigation
@@ -28,9 +27,34 @@ namespace Songhay.StudioFloor.Desktop.ViewModels
             get { return this._clientContentRegionNavigation; }
         }
 
-        public ObservableCollection<DisplayItemModel> Index { get; private set; }
+        public ICollectionView IndexCollectionView
+        {
+            get { return this._indexViewSource.GetView(this._index); }
+        }
 
+        void SetupIndexViewSource()
+        {
+            this._indexViewSource = new CollectionViewSource();
+            this._indexViewSource.SortDescriptions.Add(new SortDescription("DisplayText", ListSortDirection.Ascending));
+        }
+
+        void SetupIndexWithEventing(IEventAggregator eventAggregator)
+        {
+            this._index = new ObservableCollection<DisplayItemModel>();
+            eventAggregator
+                .GetEvent<ModuleInitializedEvent>()
+                .Subscribe(displayItem => this._index.Add(displayItem));
+        }
+
+        void SetupRegionNavigation()
+        {
+            this._regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+            this._clientContentRegionNavigation = this.GetClientContentRegionNavigation(this._regionManager);
+        }
+
+        CollectionViewSource _indexViewSource;
         IRegionManager _regionManager;
+        ObservableCollection<DisplayItemModel> _index;
         RegionNavigationViewModel _clientContentRegionNavigation;
     }
 }
